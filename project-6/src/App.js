fb3fc546c98a3f9b699759eb2bdb9bd2d3125d65
Mitchell Raznick <mraznick@Mitchells-MacBook-Air.local>
2022-07-18 16:34:54 -0400

    wrote logic for activating tabs for display

--- project-6/src/App.js
+++ project-6/src/App.js
@@ -1,37 +1,10 @@
-import Synopsis from "./Synopsis.jsx";
-import SeriesInfo from "./SeriesInfo.jsx";
-import Episodes from "./Episodes.jsx";
-import Gallery from "./Gallery.jsx";
-import Tabs from "./Tabs"
-import { Link, Routes, Route } from "react-router-dom";
+import Tabs from "./Tabs";
 import Styles from "./App.css";
 
 function App() {
-
   return (
-
     <div id="parent-container">
-
-      <nav>
-<Tabs />
-      </nav>
-
-      <div data-tab-content id="synopsis">
-        <Synopsis />
-      </div>
-
-      <div data-tab-content id="series-info">
-        <SeriesInfo />
-      </div>
-
-      <div data-tab-content id="episodes">
-        <Episodes />
-      </div>
-
-      <div data-tab-content id="gallery">
-        <Gallery />
-      </div>
-
+      <Tabs />
     </div>
   );
 }
--- project-6/src/Tabs.jsx
+++ project-6/src/Tabs.jsx
@@ -1,16 +1,68 @@
+import Synopsis from "./Synopsis";
+import SeriesInfo from "./SeriesInfo";
+import Episodes from "./Episodes";
+import Gallery from "./Gallery";
+import { useState } from "react";
+import "./App.css"
+
 const Tabs = () => {
+
+const [current, setCurrent] = useState(1)
+
+  const changeTab = (index) => {
+    setCurrent(index)
+  }
+  
   return (
-    <div>
-            <nav>
-        <ul className="tabs">
-          <li data-tab-target="synopsis">Synopsis</li>
-          <li data-tab-target="series-info-tab">Series Info</li>
-          <li data-tab-target="episodes-tab">Episodes</li>
-          <li data-tab-target="gallery-tab">Art Gallery</li>
-        </ul>
+
+    <div id="parent-div">
+
+      <nav>
+
+        <div className="tabs-container">
+          <div
+            className={current === 1 ? "tab active-tab" : "tab"}
+          onClick={() => changeTab(1)}
+          >Synopsis</div>
+
+          <div
+            className={current === 2 ? "tab active-tab" : "tab"}
+            onClick={() => changeTab(2)}
+          >Series Info</div>
+
+          <div
+            className={current === 3 ? "tab active-tab" : "tab"}
+            onClick={() => changeTab(3)}
+          >Episodes</div>
+
+          <div
+            className={current === 4 ? "tab active-tab" : "tab"}
+            onClick={() => changeTab(4)}
+          >Art Gallery</div>
+
+
+        </div>
+
       </nav>
+
+      
+      <div className="content active-content" id="synopsis">
+        <div><Synopsis /></div>
+      </div>
+
+      <div className="content" id="series-info">
+        <div><SeriesInfo /></div>
+      </div>
+
+      <div className="content" id="episodes">
+        <div><Episodes /></div>
+      </div>
+
+      <div className="content" id="art-gallery">
+        <div><Gallery /></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Tabs;
