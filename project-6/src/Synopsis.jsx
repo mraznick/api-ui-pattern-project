e61b310c160bb5d1cc6bde3472d4f4ebbfe81526
Mitchell Raznick <mraznick@Mitchells-MacBook-Air.local>
2022-07-19 13:20:35 -0400

    mostly finished synopsis tab

--- project-6/src/Synopsis.jsx
+++ project-6/src/Synopsis.jsx
@@ -1,10 +1,25 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 const Synopsis = () => {
+  const [synopsis, setSynopsis] = useState("");
+  const [poster, setPoster] = useState("");
+
+  useEffect(() => {
+    const apiCall = async () => {
+      const data = await axios("https://kitsu.io/api/edge/anime/");
+      setSynopsis(data.data.data[0].attributes.synopsis);
+      setPoster(data.data.data[0].attributes.coverImage.small)
+    };
+    apiCall();
+  });
   return (
     <div id="synopsis-component">
-      <h1>Synopsis</h1>
-      <h3>This will be the first tab where the title and synopsis will go.</h3>
+      <h1 id="synopsis-header">Synopsis</h1>
+      <img src={poster} id="synopsis-poster"/>
+      <p id="synopsis-paragraph">{synopsis}</p>
     </div>
-  )
+  );
 };
 
 export default Synopsis;
